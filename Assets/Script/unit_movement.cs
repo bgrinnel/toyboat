18185be42e37a25f030ebd328574df184b5835ff
bgrinnel <bgrinnell@cox.net>
2024-08-26 09:40:54 -0700

    basic navigation

--- Assets/Script/unit_movement.cs
+++ Assets/Script/unit_movement.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class unit_movement : MonoBehaviour
 {
-    Vector3 targetPos;
+    Vector3 screenPos;
+    [SerializeField] NavMeshAgent agent;
+    public LayerMask background;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,14 @@ public class unit_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonUp(1))
+        {
         
+            screenPos = Input.mousePosition;
+            Ray ray = Camera.main.ScreenPointToRay(screenPos);
+            if(Physics.Raycast(ray,  out RaycastHit hitData, 100, background)){
+                agent.destination = hitData.point;
+            }
+        }
     }
 }
