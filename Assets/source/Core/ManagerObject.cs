3b757405b98efef0ac54a7bfb171f3d4564b4913
Jefferey <47044769+JeffereyAEL@users.noreply.github.com>
2024-09-08 17:08:21 -0400

    Boundless world is working. Better yet I fixed my "ManagerObject's aren't being destroyed on LoadScene()" bug.

--- Assets/source/Core/ManagerObject.cs
+++ Assets/source/Core/ManagerObject.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using Unity;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ManagerObject<SubclassT> : MonoBehaviour where SubclassT : MonoBehaviour
 {
-    private static Dictionary<Type, MonoBehaviour> singletons = new();
+    private static Dictionary<Type, MonoBehaviour> singletons;
+
+    private bool bPersistsBetweenScenes = false;
 
     protected virtual void Awake()
     {
-        Debug.Log("ManagerObject Awake");
+        if (singletons == null) singletons = new();
         lock (singletons)
         {
             var subclass_type = typeof(SubclassT);
@@ -19,18 +22,23 @@ public class ManagerObject<SubclassT> : MonoBehaviour where SubclassT : MonoBeha
                 Debug.LogWarning($"Manager \"{subclass_type}\" already exists in the Scene. This may lead to unexpected behavior.");
                 DestroyImmediate(singletons[subclass_type]);
             }
-            Debug.Log($"New Manager Created! Name:\"{this.name}\" | typeof({typeof(SubclassT)}) | GetType({this.GetType()})");
+            Debug.Log($"New Manager Created! Name:\"{this.name}\" | typeof({subclass_type}) | GetType({this.GetType()})");
             singletons[subclass_type] = this;
         }
+        if (!bPersistsBetweenScenes) SceneManager.sceneUnloaded += DestroyOnSceneUnload;
     }
 
-    protected virtual void OnDestroyed()
+    // Set Whether this ManagerObject is not Destroyed when a new Scene becomes active
+    public void SetPersistance(bool bPersists)
+    {
+        if (bPersists && !bPersistsBetweenScenes) SceneManager.sceneUnloaded -= DestroyOnSceneUnload;
+        if (!bPersists && bPersistsBetweenScenes) SceneManager.sceneUnloaded += DestroyOnSceneUnload;
+        bPersistsBetweenScenes = bPersists;
+    }
+
+    private void OnDestroyed()
     {
         Debug.Log($"Destroying Manager: \"{typeof(SubclassT)}\"");
-        lock(singletons)
-        {
-            singletons.Remove(typeof(SubclassT));
-        }
     }
 
     public static SubclassT Get()
@@ -42,5 +50,13 @@ public class ManagerObject<SubclassT> : MonoBehaviour where SubclassT : MonoBeha
         }
 
         return (SubclassT)singletons[typeof(SubclassT)];
+    }
+
+    // The base functionality for destroying a manager. Extend and place `base.DestroyOnActiveSceneChanged` at the end for further cleanup.
+    public virtual void DestroyOnSceneUnload(Scene _old)
+    {
+        Debug.LogWarning($"SceneUnloaded: Destroying Manager \"{typeof(SubclassT)}\"");
+        singletons.Remove(typeof(SubclassT));
+        Destroy(this, .25f);
     }
 }
